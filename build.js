4dd8b8521f8f1fb061cfa9913d501e96b0f8d8a8
cdaringe <christopher.dieringer@gmail.com>
2017-06-25 14:22:44 -0700

    fix(build): install build deps onbuild

--- build.js
+++ build.js
@@ -6,6 +6,6 @@ var path = require('path')
 var uiRoot = path.resolve(__dirname, 'packages', 'ui')
 
 cp.execSync('rm -rf node_modules', { cwd: uiRoot, stdio: 'inherit' })
-cp.execSync('npm install', { cwd: uiRoot, stdio: 'inherit' })
+cp.execSync('NODE_ENV=development npm install', { cwd: uiRoot, stdio: 'inherit' })
 cp.execSync('npm run build', { cwd: uiRoot, stdio: 'inherit' })
 cp.execSync('docker build . -t dino-dna/truth.lol', { stdio: 'inherit' })
