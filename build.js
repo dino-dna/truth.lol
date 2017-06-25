ddf555952b1ba29b85d6a9e7f51a2b898cd7255c
cdaringe <christopher.dieringer@gmail.com>
2017-06-25 14:43:31 -0700

    fix(build): install build deps onbuild

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ var path = require('path')
 
 var uiRoot = path.resolve(__dirname, 'packages', 'ui')
 
-cp.execSync('rm -rf node_modules', { cwd: uiRoot, stdio: 'inherit' })
-cp.execSync('NODE_ENV=development npm install', { cwd: uiRoot, stdio: 'inherit' })
-cp.execSync('npm run build', { cwd: uiRoot, stdio: 'inherit' })
+cp.execSync('rm -f yarn.lock', { cwd: uiRoot, stdio: 'inherit' })
+cp.execSync('NODE_ENV=development yarn --ignore-optional', { cwd: uiRoot, stdio: 'inherit' })
+cp.execSync('yarn run build', { cwd: uiRoot, stdio: 'inherit' })
 cp.execSync('docker build . -t dino-dna/truth.lol', { stdio: 'inherit' })
