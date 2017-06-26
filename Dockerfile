14f88dfcb7b17b3f71467525c2aa429b3e725caa
cdaringe <christopher.dieringer@gmail.com>
2017-06-25 23:25:43 -0700

    fix(build): build UI and copy assets pre-containerization

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 ENV NODE_ENV=production
 RUN mkdir -p /api/public
 COPY ./packages/api /api
-COPY ./packages/ui/build/* /api/public/
+# COPY ./packages/ui/build/* /api/public/
 WORKDIR  /api
 RUN npm install
 CMD ["npm", "start"]
--- build.js
+++ build.js
@@ -4,8 +4,12 @@ var cp = require('child_process')
 var path = require('path')
 
 var uiRoot = path.resolve(__dirname, 'packages', 'ui')
+var apiRoot = path.resolve(__dirname, 'packages', 'api')
+var uiBuildRoot = path.resolve(uiRoot, 'build')
+var apiPublicRoot = path.resolve(apiRoot, 'public')
 
-cp.execSync('rm -f yarn.lock', { cwd: uiRoot, stdio: 'inherit' })
-cp.execSync('NODE_ENV=development yarn --ignore-optional', { cwd: uiRoot, stdio: 'inherit' })
+// WAT build runs out of mem on the droplet.
 cp.execSync('yarn run build', { cwd: uiRoot, stdio: 'inherit' })
+cp.execSync(`rm -rf ${apiPublicRoot}/*`, { stdio: 'inherit' })
+cp.execSync(`cp -r ${uiBuildRoot}/* ${apiPublicRoot}/`, { stdio: 'inherit' })
 cp.execSync('docker build . -t dino-dna/truth.lol', { stdio: 'inherit' })
