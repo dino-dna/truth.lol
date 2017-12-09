0f56257bf5c1669b0436e04746a350f810f10eab
cdaringe <christopher.dieringer@gmail.com>
2017-12-08 21:18:56 -0800

    damn(you): rust

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,33 +1,22 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 #[macro_use]
 
 extern crate lazy_static;
-extern crate hyper;
-extern crate futures;
+extern crate rocket;
 extern crate json;
-
-use hyper::header::{ContentType};
-use hyper::server::{Http, Request, Response, Service};
-use hyper::{Method, StatusCode};
-use futures::future::FutureResult;
+use json::{JsonValue};
+use rocket::response::content;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
-use json::{JsonValue};
 
-struct Bullshit;
 lazy_static! {
     static ref LIES: JsonValue = {
         let path = Path::new("lies.json");
         let display = path.display();
-        let mut file = match File::open(&path) {
-            Err(_) => {
-                panic!("couldn't open {}", display)
-            },
-            Ok(file) => file,
-        };
-
+        let mut file = File::open(&path).unwrap();
         let mut lies = String::new();
-
         match file.read_to_string(&mut lies) {
             Err(_) => {
                 panic!("couldn't read {}", display)
@@ -38,34 +27,12 @@ lazy_static! {
     };
 }
 
-impl Service for Bullshit {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = FutureResult<Self::Response, Self::Error>;
-
-    fn call(&self, req: Request) -> Self::Future {
-        println!("request recieved");
-        let mut response = Response::new();
-        match (req.method(), req.path()) {
-            (&Method::Get, "/bullshit") => {
-                println!("{}", req.path());
-                response = Response::new()
-                .with_header(ContentType::json())
-                .with_body(LIES[0].dump());
-            },
-            _ => {
-                response.set_status(StatusCode::NotFound);
-            },
-        };
-
-        futures::future::ok(response)
-    }
+#[get("/bullshit")]
+fn bullshit<'a>() -> content::Json<&'a str> {
+    let dumpy = LIES.dump();
+    return content::Json(dumpy);
 }
 
 fn main() {
-    let addr = "127.0.0.1:3001".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(Bullshit)).unwrap();
-    println!("Server running on {}", addr);
-    server.run().unwrap();
+    rocket::ignite().mount("/", routes![bullshit]).launch();
 }
