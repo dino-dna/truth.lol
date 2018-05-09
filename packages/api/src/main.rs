427288afc6086ec3cb9c6385d07a461293db7545
cdaringe <christopher.dieringer@gmail.com>
2018-05-08 18:44:30 -0700

    refactor(api): drop logging because dummy

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -1,6 +1,5 @@
 extern crate log;
 extern crate actix_web;
-extern crate env_logger;
 
 use actix_web::http::{Method};
 use actix_web::middleware::{self};
@@ -12,7 +11,6 @@ static PUBLIC_DIRNAME: &str = &"public";
 fn index(_req: HttpRequest) -> Result<fs::NamedFile> {
     let path = Path::new("public/index.html");
     let filename = path.canonicalize()?;
-    println!("\nindex filename: {:?}\n", filename);
     Ok(fs::NamedFile::open(filename)?)
 }
 fn api_bullshit(_req: HttpRequest) -> Result<fs::NamedFile> {
@@ -20,14 +18,11 @@ fn api_bullshit(_req: HttpRequest) -> Result<fs::NamedFile> {
     // 60 * 60 * 24 * 7 (days)
     let path = Path::new("./bullshit.json");
     let filename = path.canonicalize()?;
-    println!("\nbs filename: {:?}\n", filename);
     Ok(fs::NamedFile::open(filename)?)
 }
 
 fn main() {
-    env::set_var("RUST_LOG", "actix_web=debug");
-    env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
+    env::set_var("RUST_LOG", "actix_web=info");
     let port = 8080;
     server::new(|| {
         App::new()
@@ -38,7 +33,6 @@ fn main() {
                     .default_handler(index))
             .default_resource(|r| {
                 // 404 for GET request
-                println!("serving default");
                 r.method(Method::GET).f(index);
                 r.route()
                     .filter(pred::Not(pred::Get()))
